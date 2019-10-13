5f67975072123706f34d0e973d28e02f59872afd
Mirian Okradze <okradzemirian@gmail.com>
2019-10-13 19:34:04 +0400

    Simple ColorsList component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,17 @@
 import React from 'react'
+import ColorsList from '../ColorsList/ColorsList'
 
-const App = () => <div>App</div>
+const App = () => (
+    <ColorsList
+        colors={[
+            '#65689D',
+            '#396006',
+            '#A4BE78',
+            '#91347C',
+            '#122785',
+            '#63EF8A',
+        ]}
+    />
+)
 
 export default App
---
+++ src/components/ColorsList/ColorsList.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const ColorsList = ({ colors }) => colors.map(color => <p>{color}</p>)
+
+export default ColorsList
