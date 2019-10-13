046bae2d6d49d49ff3b5b948243dd8d65070b3b7
Mirian Okradze <okradzemirian@gmail.com>
2019-10-13 22:36:55 +0400

    Added copying feature for Color component

--- src/components/Color/Color.js
+++ src/components/Color/Color.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import styles from './Color.module.scss'
 
-const Color = ({ color }) => (
-    <div className={styles.Color} style={{ background: color }}></div>
-)
+const Color = ({ color }) => {
+    const copyToClipboard = e => {
+        e.target.select()
+        document.execCommand('copy')
+    }
+
+    return (
+        <div className={styles.Color} style={{ background: color }}>
+            <input
+                className={styles.ColorValue}
+                type="text"
+                value={color}
+                onClick={e => copyToClipboard(e)}
+            />
+        </div>
+    )
+}
 
 export default Color
